9856adad0e1b524089015bf905a1bf0813ceb5e5
Ashutosh Bhardwaj <bhardwajashu96@hotmail.com>
2019-09-09 08:54:28 +0530

    Implement FAQs section

---
+++ src/components/FAQs/FAQs.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Styled from 'styled-components'
+import Accordion from '../Accordion'
+import questions from './questions'
+import {extended} from '../../constants/colors'
+
+const FAQsWrapper = Styled.div`
+  max-width: 770px;
+  /* min-height: 454px; */
+  margin: auto;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px 0 rgba(219, 221, 227, 0.5);
+  background-color: #ffffff;
+
+  padding: 24px;
+
+  & hr {
+    border-color: ${extended.purple.four};
+  }
+`
+
+function FAQs() {
+  return (
+    <FAQsWrapper>
+      {questions.map(question => (
+        <React.Fragment key={question.id}>
+          <Accordion {...question}/>
+          {question.id < questions.length - 1 && <hr />}
+        </React.Fragment>
+      ))}
+    </FAQsWrapper>
+  )
+}
+
+export default FAQs
+
+FAQs.propTypes = {
+  title: PropTypes.string.isRequired
+}
+FAQs.defaultTypes = {
+  title: PropTypes.string.isRequired
+}
---
+++ src/components/FAQs/questions.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 0,
+    question: 'How long are your contracts?',
+    answer: `Employee access to the Uprise digital platform is renewed annually.
+     Coaching support hours are pre-purchased and do expire. 
+     We review usage and availability quarterly to ensure a seamless delivery of support for employees. `
+  },
+  {
+    id: 1,
+    question: 'When and how do employees access coaching and support?',
+    answer: `Employee access to the Uprise digital platform is renewed annually.
+     Coaching support hours are pre-purchased and do expire. 
+     We review usage and availability quarterly to ensure a seamless delivery of support for employees. `
+  },
+  {
+    id: 2,
+    question: 'Do you provide face to face therapy?',
+    answer: `Employee access to the Uprise digital platform is renewed annually.
+     Coaching support hours are pre-purchased and do expire. 
+     We review usage and availability quarterly to ensure a seamless delivery of support for employees. `
+  },
+  {
+    id: 3,
+    question: 'When will I see results?',
+    answer: `Employee access to the Uprise digital platform is renewed annually.
+     Coaching support hours are pre-purchased and do expire. 
+     We review usage and availability quarterly to ensure a seamless delivery of support for employees. `
+  },
+  {
+    id: 4,
+    question: 'How soon can we start?',
+    answer: `Employee access to the Uprise digital platform is renewed annually.
+     Coaching support hours are pre-purchased and do expire. 
+     We review usage and availability quarterly to ensure a seamless delivery of support for employees. `
+  },
+]
