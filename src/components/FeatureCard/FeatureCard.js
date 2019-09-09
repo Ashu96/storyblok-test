024f09ae2da58f404810c8f6bcfcc6cae81a2796
Ashutosh Bhardwaj <bhardwajashu96@hotmail.com>
2019-09-09 12:19:11 +0530

    Implement Feature Card

---
+++ src/components/FeatureCard/FeatureCard.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Styled from 'styled-components'
+import { backgrounds } from '../../constants/colors'
+import { Heading3, BodyText } from '../../styles/text'
+
+const FeatureCardWrapper = Styled.div`
+  width: auto;
+  height: 590px;
+  background-color: ${backgrounds.fadedPurple};
+  padding: 40px;
+  text-align: center;
+
+  margin-top: 16px;
+
+  & .image-container {
+    margin: 40px 0px;
+    & img {
+      height: 171px;
+    }
+  }
+
+  & .heading {
+    display: flex;
+    flex-direction: column;
+
+    margin-bottom: 64px;
+  }
+
+  @media (min-width: 768px) {
+    min-width: 300px;
+    height: 620px;
+  }
+
+  @media (min-width: 1220px) {
+    width: 345px;
+    height: 590px;
+  }
+`
+
+function FeatureCard({ blok }) {
+  const {title, media, body} = blok
+	return (
+		<FeatureCardWrapper>
+      <Heading3>{title}</Heading3>
+			<div className="image-container">
+				{media && <img src={media} alt={title} />}
+				{/* {media && <Img fluid={media} alt={title} />} */}
+			</div>
+			<BodyText>{body}</BodyText>
+		</FeatureCardWrapper>
+	)
+}
+
+export default FeatureCard
+
+FeatureCard.propTypes = {
+	title: PropTypes.string.isRequired
+}
+FeatureCard.defaultTypes = {
+	title: PropTypes.string.isRequired
+}
