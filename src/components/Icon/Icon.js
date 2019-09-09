8032418b7946929b8c37b546f16cb5ea20415eab
Ashutosh Bhardwaj <bhardwajashu96@hotmail.com>
2019-09-09 08:54:49 +0530

    Add Icon component

---
+++ src/components/Icon/Icon.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Styled from 'styled-components'
+
+import ICON_LIST from './IconList'
+
+// const IconWrapper = Styled.div``
+
+function Icon({ name, fill, width, rotate, ...props }) {
+  const Svg = ICON_LIST[name]
+  const styles = rotate ? { transform: `rotate(${rotate}deg)` } : {}
+
+  return <Svg fill={fill} width={width} style={styles} {...props} />
+}
+
+export default Icon
+
+Icon.propTypes = {
+  name: PropTypes.string,
+  width: PropTypes.string,
+  fill: PropTypes.string
+}
+
+Icon.defaultProps = {
+  name: 'chevron',
+  width: '24px',
+  fill: '#000'
+}
