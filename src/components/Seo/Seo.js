ed068985f4643a749eee87f6a4f07fecbb509f05
Ashutosh Bhardwaj <bhardwajashu96@hotmail.com>
2019-09-10 21:11:05 +0530

    Update SEO component with tags

--- src/components/Seo/Seo.js
+++ src/components/Seo/Seo.js
@@ -3,49 +3,63 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 function Seo({ blok }) {
-  const { title, description, lang = 'en', keywords = [], meta= '' } = blok
+  const { lang = 'en', keywords = [], meta = {} } = blok
+  const {
+    description = 'Uprise is a proactive and science-based Employee Assistance Program (EAP) provider in Australia.',
+    og_description = 'Uprise is a proactive and science-based Employee Assistance Program (EAP) provider in Australia.',
+    og_image = '//a.storyblok.com/f/65490/600x170/de9faa5018/upriselogo.jpg',
+    og_title = 'Uprise Employee Assistance',
+    plugin = 'seo_metatags',
+    title = 'Uprise Employee Assistance',
+    twitter_description = 'Uprise is a proactive and science-based Employee Assistance Program (EAP) provider in Australia.',
+    twitter_image = '//a.storyblok.com/f/65490/600x170/de9faa5018/upriselogo.jpg',
+    twitter_title = 'Uprise Employee Assistance'
+  } = meta
   return (
     <Helmet
       htmlAttributes={{
         lang
       }}
       title={title}
-      // titleTemplate={`%s | ${data.site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: description
-        },
-        {
-          property: `og:title`,
-          content: title
-        },
-        {
-          property: `og:description`,
-          content: description
-        },
-        {
-          property: `og:type`,
-          content: `website`
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`
-        },
-        {
-          name: `twitter:creator`,
-          content: 'twitter author'
-        },
-        {
-          name: `twitter:title`,
-          content: 'twitter title'
-        },
-        {
-          name: `twitter:description`,
-          content: 'twitter description'
-        }
-      ]
-        .concat(
+      meta={
+        [
+          {
+            name: `description`,
+            content: description
+          },
+          {
+            property: `og:title`,
+            content: og_title
+          },
+          {
+            property: `og:description`,
+            content: og_description
+          },
+          {
+            property: `og:image`,
+            content: og_image
+          },
+          {
+            property: `og:type`,
+            content: `website`
+          },
+          {
+            name: `twitter:card`,
+            content: `summary`
+          },
+          {
+            name: `twitter:image`,
+            content: twitter_image
+          },
+          {
+            name: `twitter:title`,
+            content: twitter_title
+          },
+          {
+            name: `twitter:description`,
+            content: twitter_description
+          }
+        ].concat(
           keywords.length > 0
             ? {
                 name: `keywords`,
@@ -53,7 +67,8 @@ function Seo({ blok }) {
               }
             : []
         )
-        .concat(meta)}
+        // .concat(meta)
+      }
     />
   )
 }
