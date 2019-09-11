554031e11e973e6d028d1c958ad9510538adfa27
Ashutosh Bhardwaj <bhardwajashu96@hotmail.com>
2019-09-11 16:01:41 +0530

    Add `ContentWrapper`

--- src/styles/text.js
+++ src/styles/text.js
@@ -51,7 +51,7 @@ export const Heading4 = Styled.h4`
 
 export const BodyText = Styled.p`
   font-family: ${props =>
-		props.bold ? 'Proxima Nova Semibold' : 'Proxima Nova'};
+    props.bold ? 'Proxima Nova Semibold' : 'Proxima Nova'};
   font-size: 18px;
   font-weight: ${props => (props.bold ? 600 : 'normal')};
   font-style: normal;
@@ -82,4 +82,68 @@ export const Label = Styled.p`
   line-height: 1.7;
   letter-spacing: normal;
   color: ${props => (props.color ? props.color : extended.charcoal.one)};
+`
+
+export const ContentWrapper = Styled.div`
+  & img {
+    width: 100%;
+  }
+
+  & li {
+    font-family: ${props =>
+      props.bold ? 'Proxima Nova Semibold' : 'Proxima Nova'};
+    font-size: 18px;
+    font-weight: ${props => (props.bold ? 600 : 'normal')};
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.56;
+    letter-spacing: normal;
+    text-align: ${props => (props.textCenter ? 'center' : 'inherit')};
+    color: ${props => (props.color ? props.color : extended.charcoal.one)};
+    
+  }
+
+  & p {
+    font-family: ${props =>
+      props.bold ? 'Proxima Nova Semibold' : 'Proxima Nova'};
+    font-size: 18px;
+    font-weight: ${props => (props.bold ? 600 : 'normal')};
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.56;
+    letter-spacing: normal;
+    text-align: ${props => (props.textCenter ? 'center' : 'inherit')};
+    color: ${props => (props.color ? props.color : extended.charcoal.one)};
+    
+    margin-bottom: 50px;
+  }
+
+  & h1, h2, h3, h4 {
+    margin-bottom: 20px;
+  }
+
+  & h2 {
+    font-family: 'Proxima Nova Semibold';
+    font-size: 32px;
+    font-weight: 600;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.25;
+    letter-spacing: normal;
+    text-align: ${props => (props.textCenter ? 'center' : 'inherit')};
+    color: ${primary.charcoal};
+  }
+
+  & h3 {
+    font-family: 'Proxima Nova Semibold';
+    font-size: 24px;
+    font-weight: 600;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 1.25;
+    letter-spacing: normal;
+    text-align: ${props => (props.textCenter ? 'center' : 'inherit')};
+    color: ${primary.charcoal};Z
+  }
+
 `
