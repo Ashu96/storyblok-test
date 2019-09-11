1b71897e8dcb2f56ca968ced250a59e9e407b403
Ashutosh Bhardwaj <bhardwajashu96@hotmail.com>
2019-09-11 16:05:49 +0530

    Update styles

--- src/components/SingleMediaWithCaptionAndParagraph/SingleMediaWithCaptionAndParagraph.js
+++ src/components/SingleMediaWithCaptionAndParagraph/SingleMediaWithCaptionAndParagraph.js
@@ -1,20 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Styled from 'styled-components'
-// import Img from 'gatsby-image'
-// import { OutLineButton, PrimaryButton } from '../../styles/buttons'
+import ReactMarkdown from 'react-markdown'
 import { Col } from '../../styles/grid'
-import { Heading2, BodyText, Note } from '../../styles/text'
-// import { backgrounds } from '../../constants/colors'
+import { Heading2, BodyText, Note, ContentWrapper } from '../../styles/text'
+import { primary } from '../../constants/colors'
 
 const SingleMediaWithCaptionAndParagraphWrapper = Styled.div`
-  flex-direction: ${props => props.imageFirst ? 'row-reverse' : 'row'};
+  flex-direction: ${props => (props.imageFirst ? 'row-reverse' : 'row')};
 
   & img {
     width: 100%;
+    max-width: ${props => (props.timeline ? '200px' : 'inherit')};
+    max-height: ${props => (props.timeline ? '200px' : 'inherit')};
+  }
+
+  & .media {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+
+  & .info:before {
+    content: '';
+    display: ${props => props.timeline ? 'block' : 'none'};
+    width: 16px;
+    height: 16px;
+    background-color: ${primary.purple};
+    position: absolute;
+    border-radius: 50%;
+    left: ${props => !props.imageFirst ? '-16px' : 'inherit'};
+    right: ${props => props.imageFirst ? '0' : 'inherit'};
+    top: 10px;
   }
 
   & .info {
+    position: relative;
     margin-top: 16px;
 
     & p {
@@ -33,21 +54,31 @@ function SingleMediaWithCaptionAndParagraph({
   body,
   stepNumber,
   showStepNumber,
-  media
+  media,
+  timeline,
+  content
 }) {
   return (
     <SingleMediaWithCaptionAndParagraphWrapper
       className="row"
       imageFirst={stepNumber % 2 === 0}
+      timeline={timeline}
     >
-      <Col className="col-12 col-lg-6">
-        {media && <img src={media} alt={title} />}
-        {/* {image && <Img fluid={image} alt={title} />} */}
+      <Col className="col-12 col-lg-6 align-self-center">
+        <div className="media">
+          {media && <img src={media} alt={title} />}
+          {/* {image && <Img fluid={image} alt={title} />} */}
+        </div>
       </Col>
       <Col className="col-lg-6 align-self-center info">
         {stepNumber && showStepNumber && <Note>{`STEP ${stepNumber}`}</Note>}
         <Heading2>{title}</Heading2>
         <BodyText>{body}</BodyText>
+        {content && (
+          <ContentWrapper>
+            <ReactMarkdown source={content} />
+          </ContentWrapper>
+        )}
       </Col>
     </SingleMediaWithCaptionAndParagraphWrapper>
   )
