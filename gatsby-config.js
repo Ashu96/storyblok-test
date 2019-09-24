22dbdeb07ae3fafe2e1a0d05c82db0c602da90d9
Ashutosh Bhardwaj <bhardwajashu96@hotmail.com>
2019-09-24 16:57:28 +0530

    Add `gatsby-plugin-manifest` and `gatsby-plugin-offline` for PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Default Starter'
   },
   plugins: [
     {
@@ -18,8 +18,28 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Uprise.co`,
+        short_name: `Uprise`,
+        description: `Uprise is a proactive and science-based Employee Assistance Program (EAP) provider in Australia.`,
+        lang: `en`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#7d60ff`,
+        display: `standalone`,
+        icon: 'src/images/logo.webp'
+      }
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/about-us/`, `/projects/*`]
+      }
     }
-  ],
+  ]
 }
