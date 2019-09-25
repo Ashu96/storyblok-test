7e07543aa3f135cb73743b492a10ed072b2cc0de
Ashutosh Bhardwaj <bhardwajashu96@hotmail.com>
2019-09-25 09:17:38 +0530

    Add comma images

--- src/components/Testimony/Testimony.js
+++ src/components/Testimony/Testimony.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Img from 'gatsby-image'
+import {useStaticQuery, graphql} from 'gatsby'
 import Styled from 'styled-components'
 import ReactMarkDown from 'react-markdown'
 import { BodyText } from '../../styles/text'
 import { backgrounds, secondary } from '../../constants/colors'
 
 const TestimonyWrapper = Styled.div`
+  position: relative;
   width: 300px;
   height: 300px;
   border-radius: 10px;
@@ -26,11 +29,54 @@ const TestimonyWrapper = Styled.div`
   & .from {
     color: ${backgrounds.white};
   }
+
+  & .comma {
+    position: absolute;
+    top: -20px;
+    left: 25px;
+  }
 `
 
 function Testimony({ message, from, backgroundColor }) {
+  const data = useStaticQuery(graphql`
+    {
+      purple: file(relativePath: { eq: "purple.png" }) {
+        childImageSharp {
+          fixed(width: 52) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      },
+      electricBlue: file(relativePath: { eq: "electricBlue.png" }) {
+        childImageSharp {
+          fixed(width: 52) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      },
+      electricPurple: file(relativePath: { eq: "electricPurple.png" }) {
+        childImageSharp {
+          fixed(width: 52) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
+  let image = 'purple'
+  if (backgroundColor === '#39aeff') {
+    image = 'electricBlue'
+  } else if (backgroundColor === '#9db4ff') {
+    image = 'electricPurple'
+  }
+
+
   return (
     <TestimonyWrapper backgroundColor={backgroundColor}>
+      <div className='comma'>
+        <Img fixed={data[image].childImageSharp.fixed} alt=''/>
+      </div>
       <BodyText className="message" color={backgrounds.white}>
         {message}
       </BodyText>
