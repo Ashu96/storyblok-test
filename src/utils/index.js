99e57fb8d59f7803d7152352258085fc7c611d0d
Ashutosh Bhardwaj <bhardwajashu96@hotmail.com>
2019-09-25 10:36:30 +0530

    Implement `useHeader` hook

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import { navigate } from 'gatsby'
+import { navigate, useStaticQuery, graphql } from 'gatsby'
 import {
   BUTTON_TYPES,
-  NAV_ITEM_TYPES,
-  // SECTION_TYPES,
-  // CARD_TYPES
+  NAV_ITEM_TYPES
 } from '../constants'
 import {
   PrimaryButton,
@@ -12,45 +10,12 @@ import {
   LinkButton,
   OutLineButton
 } from '../styles/buttons'
-// import HeroSection from '../components/HeroSection'
-// import ContentWithImageList from '../components/ContentWithImageList'
-// import TourSection from '../components/tourSection'
-// import HeadingWithSingleMediaAndButton from '../components/HeadingWithSingleMediaAndButton'
-// import SingleMediaWithParagraphAndLink from '../components/SingleMediaWithParagraphAndLink'
-// import Reports from '../components/Reports'
-// import SectionWithCards from '../components/SectionWithCards'
 import {
   NavLogo,
   NavLink,
   NavButton,
   NavDropDown
 } from '../components/Header/NavItems'
-// import FeaturedCard from '../components/FeatureCard'
-// import SupportCard from '../components/SupportCard'
-// import ContactBanner from '../components/ContactBanner'
-
-// export function getComponent(type) {
-// 	switch (type) {
-// 	case SECTION_TYPES.HERO_SECTION:
-// 		return HeroSection
-// 	case SECTION_TYPES.MULTIPLE_MEDIA_WITH_CAPTION:
-// 		return ContentWithImageList
-// 	case SECTION_TYPES.SINGLE_MEDIA_WITH_CAPTION_AND_PARAGRAPH:
-// 		return TourSection
-// 	case SECTION_TYPES.WITH_CARDS:
-// 		return SectionWithCards
-// 	case SECTION_TYPES.HEADING_WITH_SINGLE_MEDIA_AND_BUTTON:
-// 		return HeadingWithSingleMediaAndButton
-// 	case SECTION_TYPES.SINGLE_MEDIA_WITH_PARAGRAPH_AND_LINK:
-// 		return SingleMediaWithParagraphAndLink
-// 	case SECTION_TYPES.REPORTS:
-// 		return Reports
-// 	case SECTION_TYPES.CONTACT_BANNER:
-// 		return ContactBanner
-// 	default:
-// 		return () => <h1>No component found</h1>
-// 	}
-// }
 
 export function getButton(type) {
   switch (type) {
@@ -67,41 +32,12 @@ export function getButton(type) {
   }
 }
 
-// function getStrapiURL() {
-// 	console.log(`Env: ${process.env.NODE_ENV}`)
-// 	return process.env.NODE_ENV === 'development'
-// 		? 'http://localhost:1337'
-// 		: 'http://testing-marketing.uprise.co'
-// }
-
-// export function getPublicURL(url) {
-// 	return `${getStrapiURL()}${url}`
-// }
-
-// export function extractQueryData({ data, id }) {
-// 	if (!data) {
-// 		return null
-// 	}
-
-// 	const filteredDataArray = data.edges.filter(
-// 		edge => edge.node.strapiId === id.trim()
-// 	)
-
-// 	const { node } =
-// 		filteredDataArray && filteredDataArray.length && filteredDataArray[0]
-
-// 	if (!node) {
-// 		return null
-// 	}
-
-// 	return node
-// }
 
 export function getSlugFromTitle(title) {
-	return title
-		.toLowerCase()
-		.replace(/[^\w ]+/g, '')
-		.replace(/ +/g, '-')
+  return title
+    .toLowerCase()
+    .replace(/[^\w ]+/g, '')
+    .replace(/ +/g, '-')
 }
 
 export function getNavItem(item) {
@@ -117,25 +53,14 @@ export function getNavItem(item) {
   }
 }
 
-// export function getCard(type) {
-// 	switch (type) {
-// 	case CARD_TYPES.FEATURE_CARD:
-// 		return FeaturedCard
-// 	case CARD_TYPES.SUPPORT_CARD:
-// 		return SupportCard
-// 	default:
-// 		return FeaturedCard
-// 	}
-// }
 
 export function isExternal(url) {
   return url.includes('http')
 }
 
-
 export function navigateTo(link, { fn } = {}) {
   if (isExternal(link)) {
-    window.open(link, "_blank")
+    window.open(link, '_blank')
   } else if (link.includes('video') && fn) {
     fn()
   } else {
@@ -162,3 +87,33 @@ export function getDateWithoutTime(date) {
 }
 
 export const VideoPlayerContext = React.createContext({ playVideo: false })
+
+export function useHeader() {
+  const data = useStaticQuery(graphql`
+    {
+      allStoryBlokEntry: allStoryblokEntry(
+        filter: { full_slug: { eq: "home" } }
+      ) {
+        edges {
+          node {
+            id
+            name
+            full_slug
+            published_at(fromNow: true)
+            slug
+            field_component
+            content
+          }
+        }
+      }
+    }
+  `)
+  // Get header
+  const { allStoryBlokEntry } = data
+  const { node } = allStoryBlokEntry.edges.find(
+    ({ node }) => node.field_component === 'page'
+  )
+  const content = JSON.parse(node.content)
+  const header = content.body.find(item => item.component === 'header')
+  return header
+}
