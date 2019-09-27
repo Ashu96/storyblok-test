26cee7b58a2db9e6733ea18f8f00691cc8144d52
Ashutosh Bhardwaj <bhardwajashu96@hotmail.com>
2019-09-27 10:14:18 +0530

    Add google-tag-manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,26 @@ module.exports = {
       options: {
         transitionTime: 500
       }
+    },
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'UA-69911631-4',
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // data layer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: 'gatsby' },
+
+        // Specify optional GTM environment details.
+        gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING',
+        gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME',
+        dataLayerName: 'YOUR_DATA_LAYER_NAME'
+      }
     }
   ]
 }
