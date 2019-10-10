ae0c43e7001bfe560426dfd81441853ff570d87c
Ashutosh Bhardwaj <bhardwajashu96@hotmail.com>
2019-10-10 09:01:09 +0530

    Add `gatsby-plugin-robots-txt`

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
